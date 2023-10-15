b3f7f563f01b4e6d0f309a38f0ecb53d9dad63f3
Nikios Wallace <nikiosmwallace@gmail.com>
2023-10-14 22:32:17 -0400

    added table

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 console.log("helloworld")
-prompt("Please enter your order","vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+const userInput = prompt("Please enter your order","vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+
+const flavors = userInput.split(',');
+ const flavorCounts = {};
+
+flavors.forEach(function(flavor) {
+    flavor = flavor.trim(); 
+    if (flavorCounts[flavor]) {
+      flavorCounts[flavor]++;
+    } else {
+      flavorCounts[flavor] = 1;
+    }
+  });
+  
+  
+  console.table(flavorCounts);
+
